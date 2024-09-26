730dbb6cf883621aacd4ae504f5970f00bd8d4d0
Sorav Malviya <sorav@swageazy.com>
2024-09-26 23:41:26 +0530

    explicit and implicit types learnt

---
+++ Day-4-26-09-24/main.py
@@ -0,0 +1,8 @@
+a = "1"
+b = "2"
+print(int(a) + int(b)) # explicit
+print(a + b) # implicit 
+
+# Explicit Conversion- The conversion of one data type into another data type, done via developer as per the requirement is known as explicit type conversion
+
+# Implicit Conversion- The conversion of one data type is converted into other by the python interpreter itself(automatically). This is called, implicit data type.
