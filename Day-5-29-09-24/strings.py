868fe0946fdcd0a21c5866b99d3103d8977902db
Sorav Malviya <49089983+soravvv@users.noreply.github.com>
2024-09-29 22:48:20 +0530

    strings and loops implemented

---
+++ Day-5-29-09-24/strings.py
@@ -0,0 +1,14 @@
+name = "Sorav"
+friend = "Ankush"
+anotherfriend = "Viswajeet"
+multiLines = '''He said,
+Hi Sorav
+I am good
+"I want eat an apple"'''
+
+# print("Hello, " + name)
+# print(multiLines)
+# print(name[0])
+
+for character in multiLines:
+    print(character)
