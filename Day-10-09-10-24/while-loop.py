cd1d07504e73a1aa723f666247bd45764b6dccd0
Sorav Malviya <49089983+soravvv@users.noreply.github.com>
2024-10-09 22:58:12 +0530

    while loop learnt

---
+++ Day-10-09-10-24/while-loop.py
@@ -0,0 +1,12 @@
+i = int(input("Enter the number:"))
+while(i < 3):
+    print(i)
+    i = i + 1
+print("Done with the loop")
+
+count = 5
+while(count > 0):
+    print(count)
+    count = count - 1
+else:
+    print("I am inside while else block")
