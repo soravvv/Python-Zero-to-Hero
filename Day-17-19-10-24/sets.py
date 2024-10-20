acd768d0fabf8468bda8bb83c33adccc03c7bb75
Sorav Malviya <49089983+soravvv@users.noreply.github.com>
2024-10-20 22:38:27 +0530

    19th date learnt recursion and sets

---
+++ Day-17-19-10-24/sets.py
@@ -0,0 +1,13 @@
+# Set does not maintain value index or order
+
+s = {2, 4, 2, 6}
+print(s)
+
+info = {"Carla", 19, False, 5.9, 19}
+print(info)
+
+sorav = set()
+print(sorav)
+
+for value in info:
+    print(info)
