c5cb1d83cbd4a0b74e58434fe4724e1f2cfe2183
Sorav Malviya <49089983+soravvv@users.noreply.github.com>
2024-10-28 23:04:50 +0530

    error handling of specifics error

--- Day-20-28-10-24/exception-handling.py
+++ Day-20-28-10-24/exception-handling.py
@@ -8,3 +8,12 @@ except:
         print("Invalid input")
 
 print("Some lines of code")
+
+try:
+     num = int(input("Enter an integer: "))
+     a = [6, 3]
+     print(a[num])
+except ValueError:
+     print("Number entered is not an integer.")
+except IndexError:
+     print("Index Error")
